2b92c9f38d3b5af8b799f47454ad8a1b323628fb
Louis Hoang <hoang_t1@denison.edi>
2022-03-14 01:21:41 -0400

    Backtracking algorithm

--- sudoku.py
+++ sudoku.py
@@ -34,7 +34,9 @@ def find_empty(board):
     for i in range(len(board)):
         for j in range(len(board[0])):
             if board[i][j] == 0:
-                return (i,j) #row column 
+                return (i, j)  # row, col
+
+    return None
 
 def check_valid(board,num,loc):
     # check row
@@ -53,9 +55,34 @@ def check_valid(board,num,loc):
     box_x = loc[1] //3 #(box_x = 0|1|2)
     box_y = loc[0] //3 #(box_y = 0|1|2)
 
-    for i in range(box_y*3,box_y*3+3):
-        for j in range(box_x*3,box_x*3+3):
+    for i in range(box_y*3, box_y*3+3):
+        for j in range(box_x*3, box_x*3+3):
             if board[i][j] == num and (i,j) != loc:
                 return False 
 
     return True
+
+
+
+def backtrack_solve(board):
+    find = find_empty(board)
+    if not find:
+        return True
+    else:
+        row, column = find 
+
+    for i in range(1,10):
+        if check_valid(board,i,(row,column)):
+            board[row][column] = i
+
+            if backtrack_solve(board):
+                return True
+        
+            board[row][column] = 0 #reset the entry 
+
+    return False 
+
+print_board(sample)
+backtrack_solve(sample)
+print("___________________")
+print_board(sample)
